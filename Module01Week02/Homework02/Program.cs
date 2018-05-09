16dabfa4ca2893a6b4234e709246d837e2d0ed0c
ncomanici <ncomanici@pentalog.com>
2018-05-09 13:15:02 +0300

    initial commit

---
+++ Module01Week02/Homework02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implement homework 2
+        }
+    }
+}
